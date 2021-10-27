8ac17f882540976f201f57b8c6cf71d60c3397e9
m1sht1 <sanj.mins@gmail.com>
2021-10-27 17:26:34 -0400

    added project dependencies to package

---
+++ build/precache-manifest.36e71723299249728890b3ec8136fd04.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc5c71e02c0fb30b7f97",
+    "url": "/Videospirit/COMP3104/static/css/main.23139309.chunk.css"
+  },
+  {
+    "revision": "fc5c71e02c0fb30b7f97",
+    "url": "/Videospirit/COMP3104/static/js/main.fc5c71e0.chunk.js"
+  },
+  {
+    "revision": "4faf9ffe7ce6dbbd5110",
+    "url": "/Videospirit/COMP3104/static/js/runtime~main.4faf9ffe.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/Videospirit/COMP3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "7db704dc6afef653a33c98cfd9c416bb",
+    "url": "/Videospirit/COMP3104/index.html"
+  }
+];
